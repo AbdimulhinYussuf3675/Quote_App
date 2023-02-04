9d9171ecc9ca9ac37070e6fee09da4dc9a3ae4bf
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-02-04 12:28:18 +0300

    Solve linter error

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,30 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    devServer: {
-        static: './dist',
-      },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html'
-        }),
-    ],
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 
-    module: {
-        rules: [{
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        }, ],
-    },
+  module: {
+    rules: [{
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    }],
+  },
 
-    optimization: {
-        runtimeChunk: 'single',
-      },
+  optimization: {
+    runtimeChunk: 'single',
+  },
 
-      mode: 'development',
+  mode: 'development',
 };
